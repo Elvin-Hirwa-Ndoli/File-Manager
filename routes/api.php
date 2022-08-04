e58e0b2a0903a8e0a9d9d22ef94485e67ed33b7e
Elvin-Hirwa-Ndoli <elvin.h@academicbridge.xyz>
2022-08-04 15:16:01 +0200

    Adding File controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Sanctum;
@@ -20,10 +21,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-
-    
-   
+    return $request->user();   
 });
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/upload', [FileController::class, 'upload'])->middleware('auth:sanctum');
